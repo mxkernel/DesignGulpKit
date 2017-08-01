6e882c2d85e88f6f1d59d70c81cb57e94bad1a26
GU <gu@designact.tw>
2017-08-01 15:53:09 +0800

    init

---
+++ config.rb
@@ -0,0 +1,30 @@
+activate :directory_indexes
+
+set :relative_links, true
+
+page "/*.xml", layout: false
+page "/*.json", layout: false
+page "/*.txt", layout: false
+page "/404.html", directory_index: false
+
+set :css_dir, "css"
+set :images_dir, "img"
+set :js_dir, "js"
+
+configure :build do
+  activate :external_pipeline,
+    name: :gulp,
+    command: "npm run production",
+    source: ".tmp",
+    latency: 1
+
+  ignore "assets/javascripts/all.js"
+  ignore "assets/stylesheets/site"
+
+  activate :gzip
+
+  activate :minify_html do |html|
+    html.remove_quotes = false
+    html.remove_intertag_spaces = true
+  end
+end
